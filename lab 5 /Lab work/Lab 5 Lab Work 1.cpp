807d69d878257817e5cd313a83fa25bcc1e29e24
waqarkhan15403-sudo <waqarkhan15403@gmail.com>
2026-05-09 23:17:51 +0500

    Add files via upload

---
+++ lab 5 /Lab work/Lab 5 Lab Work 1.cpp
@@ -0,0 +1,22 @@
+// Name: Waqar khan
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0720
+// Section: A
+
+#include <iostream>     // Library used for input and output
+using namespace std;
+
+int main()
+{
+    int i = 1;  // Variable initialization, starting value of the counter
+
+    while(i <= 10)   // Loop will run until the value of i becomes greater than 10
+    {
+        cout << i << endl;  // Print the current value of i on the screen
+        i++;                // Increase the value of i by 1 in each iteration
+    }
+
+    return 0;   // End of the program
+}
+}
+
---
+++ lab 5 /Lab work/Lab 5 lab work 2.cpp
@@ -0,0 +1,21 @@
+// Name: Waqar khan
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0720
+// Section: A
+
+#include <iostream>   // Library used for input and output
+using namespace std;  // Allows using cout and cin without std::
+
+int main()
+{
+    int i = 1;   // Initialize variable i with value 1
+
+    do
+    {
+        cout << i << endl;  // Display the value of i
+        i++;                // Increase value of i by 1
+    }
+    while(i <= 10);   // Loop continues until i becomes greater than 10
+
+    return 0;   // End of the program
+}
